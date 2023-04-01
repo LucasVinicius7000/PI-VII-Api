dd8addc0d384912ffa3a4c6836a08e747f766db1
Lucas Vinicius <Lucas.elias@gow solucoes.com>
2023-04-01 15:15:37 -0300

    feature: criado camada de serviços + adicionados serviços do identity e do aspnetcore

---
+++ Services/Implementations/ServicesLayer.cs
@@ -0,0 +1,43 @@
+﻿using LocalStore.Services.Interfaces;
+using Microsoft.AspNetCore.Identity;
+
+namespace LocalStore.Services.Implementations
+{
+    public class ServicesLayer : IServicesLayer
+    {
+        #region Propriedades privadas
+
+        private readonly IConfiguration _configuration;
+        private readonly UserManager<IdentityUser> _userManager;
+        private readonly RoleManager<IdentityRole> _roleManager;
+        private readonly SignInManager<IdentityUser> _signInManager;
+
+        #endregion
+
+        #region Propriedades públicas
+
+        public IConfiguration Configuration => _configuration;
+        public UserManager<IdentityUser> UserManager => _userManager;
+        public RoleManager<IdentityRole> RoleManager => _roleManager;
+        public SignInManager<IdentityUser> SignInManager => _signInManager;
+        public UserServices User { get; }
+
+        #endregion
+
+        public ServicesLayer
+        (
+            IConfiguration configuration,
+            UserManager<IdentityUser> userManager,
+            RoleManager<IdentityRole> roleManager,
+            SignInManager<IdentityUser> signInManager
+        )
+        {
+            _userManager = userManager;
+            _roleManager = roleManager;
+            _signInManager = signInManager;
+            _configuration = configuration;
+            User ??= new UserServices(this);
+        }
+        
+    }
+}
---
+++ Services/UserServices.cs
@@ -0,0 +1,20 @@
+﻿using LocalStore.Services.Interfaces;
+
+namespace LocalStore.Services
+{
+    public class UserServices
+    {
+        private readonly IServicesLayer _services;
+
+        public UserServices
+        (
+            IServicesLayer services
+        )
+        {
+            _services = services;
+        }
+
+  
+
+    }
+}
