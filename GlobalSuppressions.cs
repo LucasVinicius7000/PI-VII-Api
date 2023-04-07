f61aa9883bbeaafa71eeab22fa6354216ccf8ab6
Lucas Vinicius <lucasvinicius7000@gmail.com>
2023-04-07 12:45:26 -0300

    feat: criado serviço de blobstorage integrado ao azure

---
+++ GlobalSuppressions.cs
@@ -0,0 +1,11 @@
+﻿// This file is used by Code Analysis to maintain SuppressMessage
+// attributes that are applied to this project.
+// Project-level suppressions either have no target or are given
+// a specific target and scoped to a namespace, type, member, etc.
+
+using System.Diagnostics.CodeAnalysis;
+
+[assembly: SuppressMessage("Style", "IDE0058:O valor da expressão nunca é usado", Justification = "<Pendente>", Scope = "member", Target = "~M:LocalStore.Program.Main(System.String[])")]
+[assembly: SuppressMessage("Style", "IDE0058:O valor da expressão nunca é usado", Justification = "<Pendente>", Scope = "member", Target = "~M:LocalStore.Infra.Data.Context.LocalStoreDbContext.OnModelCreating(Microsoft.EntityFrameworkCore.ModelBuilder)")]
+[assembly: SuppressMessage("Usage", "CA2201:Não gerar tipos de exceção reservados", Justification = "<Pendente>", Scope = "member", Target = "~M:LocalStore.Infra.BlobStorage.Implementations.BlobStorageService.UploadFile(System.String,System.String,System.IO.Stream)~System.Threading.Tasks.Task{System.String}")]
+[assembly: SuppressMessage("Style", "IDE0008:Usar o tipo explícito", Justification = "<Pendente>", Scope = "member", Target = "~M:LocalStore.Infra.BlobStorage.Implementations.BlobStorageService.UploadFile(System.String,System.String,System.IO.Stream)~System.Threading.Tasks.Task{System.String}")]
---
+++ Infra/BlobStorage/Interfaces/IBlobStorageService.cs
@@ -0,0 +1,7 @@
+﻿namespace LocalStore.Infra.BlobStorage.Interfaces
+{
+    public interface IBlobStorageService
+    {
+        Task<string> UploadFile(string fileName, string fileExtension, Stream content);
+    }
+}
